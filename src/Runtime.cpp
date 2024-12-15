d78a64a6233ce6ab433bbc6e9fbe1018724500fc
vorgestern <vorgestern@niemals>
2024-12-15 20:32:42 +0100

    Runtime.cpp: Rewritten in C++

--- src/Runtime.cpp
+++ src/Runtime.cpp
@@ -1,67 +1,10 @@
 
-/*
-* srlua.c
-* Lua interpreter for self-running programs
-* Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
-* 14 Aug 2019 14:16:21
-* This code is hereby placed in the public domain and also under the MIT license
-*/
-
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <string>
+#include <filesystem>
 #include "srglue.h"
 #include <lua.hpp>
 
-static const char*progname="srlua";
-
-static void fatal(const char*message)
-{
-    fprintf(stderr, "%s: %s\n", progname, message);
-    exit(EXIT_FAILURE);
-}
-
-static void cannot(lua_State*L, const char*what, const char*name)
-{
-    const char*why=memcmp(what, "find", 4)==0?"no glue":strerror(errno);
-    const char*message=lua_pushfstring(L, "cannot %s %s: %s", what, name, why);
-    fatal(message);
-}
-
-typedef struct { FILE*f; size_t size; char buff[512]; } State;
-
-static const char*myget(lua_State*L, void*data, size_t*size)
-{
-    State*s=(State*)data;
-    size_t n;
-    (void)L;
-    n=(sizeof(s->buff)<=s->size)?sizeof(s->buff):s->size;
-    n=fread(s->buff, 1, n, s->f);
-    s->size-=n;
-    *size=n;
-    return (n>0)?s->buff:NULL;
-}
-
-static void load(lua_State*L, const char*name)
-{
-    Glue t;
-    State S;
-    FILE*f=fopen(name, "rb");
-    int c;
-    const int size=sizeof(t);
-    if (f==NULL) cannot(L, "open", name);
-    if (fseek(f, -size, SEEK_END)!=0) cannot(L, "seek", name);
-    if (fread(&t, sizeof(t), 1, f)!=1) cannot(L, "read", name);
-    if (memcmp(t.sig, GLUESIG, GLUELEN)!=0) cannot(L, "find a Lua program in", name);
-    if (fseek(f, t.size1, SEEK_SET)!=0) cannot(L, "seek", name);
-    S.f=f; S.size=t.size2;
-    c=getc(f);
-    if (c=='#') while (--S.size>0 && c!='\n') c=getc(f);
-    ungetc(c, f);
-    if (lua_load(L, myget, &S, "=", NULL)!=0) fatal(lua_tostring(L, -1));
-    fclose(f);
-}
+using namespace std;
 
 static int msghandler(lua_State*L)
 {
@@ -77,8 +20,7 @@ static int msghandler(lua_State*L)
 
 int main(int argc, char*argv[])
 {
-    if (argv[0]==NULL) fatal("cannot locate this executable");
-    progname=argv[0];
+    const char*progname=argv[0];
     if (lua_State*L=luaL_newstate(); L!=nullptr)
     {
         luaL_openlibs(L);
@@ -90,13 +32,59 @@ int main(int argc, char*argv[])
                                     }
                                     lua_setglobal(L, "arg");
 
-        luaL_checkstack(L, argc+1, "too many arguments to script");
+        luaL_checkstack(L, argc+1, "Too many arguments to script.");
         lua_pushcfunction(L, msghandler);
-        load(L, argv[0]);
+
+        // Extract the script and push its loader function on to the stack.
+        if (FILE*k=fopen(progname, "rb"); k!=nullptr)
+        {
+            Glue signature;
+            const int sigsize=sizeof(Glue);
+            fseek(k, -sigsize, SEEK_END);
+            if (fread(&signature, sigsize, 1, k)!=1)
+            {
+                fprintf(stderr, "Cannot read signature from '%s'.\n", progname);
+                exit(1);
+            }
+            if (memcmp(signature.sig, GLUESIG, GLUELEN)!=0)
+            {
+                fprintf(stderr, "Signature not matched in '%s'.\n", progname);
+                exit(1);
+            }
+            string script(signature.size2, 0);
+            fseek(k, signature.size1, SEEK_SET);
+            if (const auto numbytes=fread(script.data(), 1, signature.size2, k); numbytes!=signature.size2)
+            {
+                fprintf(stderr, "Cannot read %ld bytes of code from '%s'.\n", signature.size2, progname);
+                exit(1);
+            }
+            fclose(k);
+            luaL_loadbufferx(L, script.c_str(), script.size(), "embedded", nullptr);
+        }
+        else
+        {
+            fprintf(stderr, "Cannot read '%s'\n", progname);
+            exit(1);
+        }
+
+        // Push further program arguments as arguments to the loader.
         for (int i=1; i<argc; i++) lua_pushstring(L, argv[i]);
-        if (lua_pcall(L, argc-1, 0, 1)!=0) fatal(lua_tostring(L, -1));
+
+        // Execute the loader in protected mode using msghandler.
+        if (lua_pcall(L, argc-1, 0, 1)!=0)
+        {
+            const auto*message=lua_tostring(L, -1);
+            fprintf(stderr, "%s\n", message);
+            exit(1);
+        }
+
         lua_close(L);
+
+        return EXIT_SUCCESS;
     }
-    else fatal("cannot create state: not enough memory");
-    return EXIT_SUCCESS;
+    else
+    {
+        fprintf(stderr, "Cannot create Lua state: Out of memory.\n");
+        exit(1);
+    }
 }
